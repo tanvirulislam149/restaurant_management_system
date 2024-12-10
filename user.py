b62eee34fb3bce323f3c65d2b360ccc8397787bc
tanvir <tanvirulislam149@gmail.com>
2024-12-10 23:15:59 +0600

    user create done

--- user.py
+++ user.py
@@ -19,3 +19,24 @@ class Employee(User):
         self.salary = salary
         self.designation = designation
 
+class Admin(User):
+    def __init__(self, name, email, phone, address):
+        super().__init__(name, email, phone, address)
+        self.employees = []
+
+    def add_employee(self, name, email, phone, address, age, salary, designation):
+        emp = Employee(name, email, phone, address, age, salary, designation)
+        self.employees.append(emp)
+        print(f"{name} is added")
+
+    def view_employees(self):
+        print("Showing Employee List: ")
+        for emp in self.employees:
+            print(emp.name, emp.email, emp.phone, emp.address)
+
+ad = Admin("karim", "karim@gmail.com", 23232323, "Dhaka")
+ad.add_employee("rahim", "rahim@gmail.com", 32433434, "Ctg", 45, 12000, "Chef")
+ad.add_employee("Sakib", "Sakib@gmail.com", 98989898, "Sylhet", 55, 50000, "Manager")
+
+ad.view_employees()
+
