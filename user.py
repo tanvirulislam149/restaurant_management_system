f51af1acaca76dc8457da1efee079f4bfd6a6320
tanvir <tanvirulislam149@gmail.com>
2024-12-10 23:05:59 +0600

    employee class done

--- user.py
+++ user.py
@@ -4,3 +4,18 @@ Users -
 2. Employee
 3. Admin
 """
+
+class User:
+    def __init__(self, name, email, phone, address):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.address = address
+
+class Employee(User):
+    def __init__(self, name, email, phone, address, age, salary, designation):
+        super().__init__(name, email, phone, address)
+        self.age = age
+        self.salary = salary
+        self.designation = designation
+
